1aa7b05a6b32af5b4784f4542301cd48473ee33b
soohy <tngus90!>
2021-05-05 19:28:55 +0900

    style: 주석 변경

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -40,63 +40,63 @@ public class BasicItemController {
     }
 
 //    @PostMapping("/add")
-//    public String addItemV1(@RequestParam String itemName, @RequestParam int price, @RequestParam Integer quantity, Model model) {
-//        Item item = new Item();
-//        item.setItemName(itemName);
-//        item.setPrice(price);
-//        item.setQuantity(quantity);
-//
-//        itemRepository.save(item);
-//
-//        model.addAttribute("item", item);
-//
-//        return "basic/item";
-//    }
-//
-//    /**
-//     * @ModelAttribute("item") Item item
-//     * model.addAttribute("item", item); 자동 추가
-//     */
+    public String addItemV1(@RequestParam String itemName, @RequestParam int price, @RequestParam Integer quantity, Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute("item") Item item
+     * model.addAttribute("item", item); 자동 추가
+     */
 //    @PostMapping("/add")
-//    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
-//        itemRepository.save(item);
-//        //model.addAttribute("item", item); //자동 추가, 생략 가능
-//
-//        return "basic/item";
-//    }
-//
-//    /**
-//     * @ModelAttribute name 생략 가능
-//     * model.addAttribute(item); 자동 추가, 생략 가능
-//     * 생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
-//     */
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+        itemRepository.save(item);
+        //model.addAttribute("item", item); //자동 추가, 생략 가능
+
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute name 생략 가능
+     * model.addAttribute(item); 자동 추가, 생략 가능
+     * 생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
+     */
 //    @PostMapping("/add")
-//    public String addItemV3(@ModelAttribute Item item) {
-//        itemRepository.save(item);
-//
-//        return "basic/item";
-//    }
-//
-//    /**
-//     * @ModelAttribute 자체 생략 가능
-//     * model.addAttribute(item) 자동 추가
-//     */
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute 자체 생략 가능
+     * model.addAttribute(item) 자동 추가
+     */
 //    @PostMapping("/add")
-//    public String addItemV4(Item item) {
-//        itemRepository.save(item);
-//
-//        return "basic/item";
-//    }
-//
-//    /**
-//     * PRG - Post/Redirect/Get
-//     */
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
+
+    /**
+     * PRG - Post/Redirect/Get
+     */
 //    @PostMapping("/add")
-//    public String addItemV5(Item item) {
-//        itemRepository.save(item);
-//
-//        return "redirect:/basic/items/" + item.getId();
-//    }
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+
+        return "redirect:/basic/items/" + item.getId();
+    }
 
     /**
      * RedirectAttributes
